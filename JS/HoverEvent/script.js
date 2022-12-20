6063a1de74494aa503f7187161600593785259ae
Magdalena Samuel <96504344+Magdalenaspace@users.noreply.github.com>
2022-12-19 21:11:10 -0800

    Add files via upload

---
+++ JS/HoverEvent/script.js
@@ -0,0 +1,14 @@
+function over(element) {
+    element.style.backgroundColor = "lime";    
+}
+    
+function out(element) {
+    element.style.backgroundColor = "silver";   
+}
+//function over(element) {
+  //  alert("mouseover");    
+//}
+    
+//function out(element) {
+   // alert("mouseout");    
+//}
